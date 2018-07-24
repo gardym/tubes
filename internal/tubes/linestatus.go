4ee380ef28d0961a771bf021bf309e32b2ac7502
Mike Gardiner <mike@gardym.io>
2018-07-24 21:17:47 +0100

    Use rgbcolors for exact tube colors

--- internal/tubes/linestatus.go
+++ internal/tubes/linestatus.go
@@ -2,54 +2,71 @@ package tubes
 
 import (
   "fmt"
-  "github.com/fatih/color"
+
+  "github.com/aybabtme/rgbterm"
 )
 
-var lineColors = map[string]*color.Color {
-  "central": color.New(color.BgHiRed),
-  "circle": color.New(color.BgYellow),
-  "piccadilly": color.New(color.BgBlue),
-  "district": color.New(color.BgGreen),
-  "metropolitan": color.New(color.BgMagenta),
-  "waterloo-city": color.New(color.BgCyan),
-  "victoria": color.New(color.BgHiBlue),
-  "hammersmith-city": color.New(color.BgHiMagenta),
-  "jubilee": color.New(color.BgHiBlack),
-  "northern": color.New(color.BgBlack),
-  "bakerloo": color.New(color.BgRed),
+type RGBColor struct {
+  R uint8
+  G uint8
+  B uint8
 }
 
-func getLineColor(id string) *color.Color {
+// http://content.tfl.gov.uk/tfl-colour-standards-issue04.pdf
+var lineColors = map[string]RGBColor {
+  "central": RGBColor{220, 36, 31},
+  "circle": RGBColor{255, 211, 41},
+  "piccadilly": RGBColor{0, 25, 168},
+  "district": RGBColor{0, 125, 50},
+  "metropolitan": RGBColor{155, 0, 88},
+  "waterloo-city": RGBColor{147, 206, 186},
+  "victoria": RGBColor{0, 152, 216},
+  "hammersmith-city": RGBColor{244, 169, 190},
+  "jubilee": RGBColor{161, 165, 167},
+  "northern": RGBColor{0, 0, 0},
+  "bakerloo": RGBColor{178, 99, 0},
+  "overground": RGBColor{239, 123, 16},
+  "elizabeth": RGBColor{147, 100, 204},
+  "dlr": RGBColor{0, 175, 173},
+}
+
+func getLineColor(id string) RGBColor {
   c, ok := lineColors[id]
 
   if ok {
     return c
   }
 
-  return color.New(color.BgWhite).Add(color.FgBlack)
+  return RGBColor{0, 0, 0}
 }
 
-func getStatusColor(status string) *color.Color {
+func getStatusColor(status string) RGBColor {
   switch status {
   case "Severe Delays", "Part Suspended":
-    return color.New(color.FgRed)
+    return RGBColor{255, 0, 0}
   case "Minor Delays":
-    return color.New(color.FgYellow)
+    return RGBColor{255, 255, 40}
   // "Good Service" is included as default
   default:
-    return color.New(color.FgGreen)
+    return RGBColor{0, 125, 50}
   }
 }
 
+func boldString(s string) string {
+  return fmt.Sprintf("\033[1m%v\033[0m", s)
+}
 func printTubeLine(line LineStatus) {
-  lineColor := getLineColor(line.Id).Add(color.Bold).SprintFunc()
-  statusColor := getStatusColor(line.Status).SprintFunc()
+  lineColor := getLineColor(line.Id)
+  statusColor := getStatusColor(line.Status)
 
   if line.Reason != "" {
     fmt.Printf("\n")
   }
 
-  fmt.Printf("%v: %v\n", lineColor(line.Name), statusColor(line.Status))
+  lineString := rgbterm.BgString(boldString(line.Name), lineColor.R, lineColor.G, lineColor.B)
+  statusString := rgbterm.FgString(line.Status, statusColor.R, statusColor.G, statusColor.B)
+
+  fmt.Printf("%v: %v\n", lineString, statusString)
 
   if line.Reason != "" {
     fmt.Printf("%v\n\n", line.Reason)
